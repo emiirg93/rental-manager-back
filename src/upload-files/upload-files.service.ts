72519e8a41bfb7ddcfb34042cacca1cd30e09a13
Emiliano Rago <emilianorago.dev@gmail.com>
2025-02-25 00:45:40 -0300

    se cambia el valor del alquiler para ver si se despliega automaticamente en heroku al hacer un push

--- src/upload-files/upload-files.service.ts
+++ src/upload-files/upload-files.service.ts
@@ -20,7 +20,7 @@ export class UploadFileService {
         extraordinarias: 0,
         totalExpensas: 0,
       },
-      alquiler: 317000,
+      alquiler: 318000,
     };
 
     for (const [i, f] of files.entries()) {
